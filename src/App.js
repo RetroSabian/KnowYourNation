cfaf67850d8fe6d3f26de1da0692c44f971db827
TMatome <46716771+TMatome@users.noreply.github.com>
2019-02-01 15:45:48 +0200

    added a command to run all tests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.scss';
-import { BrowserRouter, Route, Switch,NavLink } from "react-router-dom";
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+import { BrowserRouter, Route, Switch, NavLink } from "react-router-dom";
 
-import './App.sass';
+import "./App.sass";
 import Dummy from "./js/components/Dummy";
 import Home from "./js/components/Home/Home";
-import Navbar from './js/components/Navbar/Navbar';
+import Navbar from "./js/components/Navbar/Navbar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <BrowserRouter>
-        <div>
-          <Navbar/>
+        <BrowserRouter>
+          <div>
+            <Navbar />
             <Switch>
               <Route path="/Dummy" component={Dummy} />
               <Route component={Home} />
             </Switch>
-        </div>
+          </div>
         </BrowserRouter>
       </div>
     );
