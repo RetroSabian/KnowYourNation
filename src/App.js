1f425752b9412321d3aeecd2125463e9a738d128
NathanRetroRabbit <nngobale@retrorabbit.co.za>
2019-02-05 22:18:37 +0200

    Navbar Component completed navigation to a few of the pages, navbar icon selected dynamically based on page your viewing.
    Book page successfully implemented as well as book card fragments.

--- src/App.js
+++ src/App.js
@@ -6,17 +6,23 @@ import { BrowserRouter, Route, Switch,NavLink } from "react-router-dom";
 import './App.sass';
 import Dummy from "./js/components/Dummy";
 import Home from "./js/components/Home/Home";
+import Books from "./js/components/Books/Books";
 import Navbar from './js/components/Navbar/Navbar';
 
 class App extends Component {
   render() {
+ 
+    var loc_navBarTitle  = "KnowYourNation";
+    var loc_navbarItems = [false,true,true,true];
+    
     return (
       <div className="App">
       <BrowserRouter>
-        <div>
-          <Navbar/>
-            <Switch>
+        <div className="borderRed">
+          {/* <Navbar titleFromParent={loc_navBarTitle} navbarItems={loc_navbarItems}/> */}
+            <Switch> 
               <Route path="/Dummy" component={Dummy} />
+              <Route path="/Books" component={Books} />
               <Route component={Home} />
             </Switch>
         </div>
