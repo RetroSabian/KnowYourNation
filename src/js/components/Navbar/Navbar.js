addace7fc4c4327f3cc77bf692e8aaace6931ccb
MrMarcelM <mmyburg@retrorabbit.co.za>
2019-02-06 16:28:58 +0200

    fixed login button

--- src/js/components/Navbar/Navbar.js
+++ src/js/components/Navbar/Navbar.js
@@ -4,20 +4,20 @@ import { BrowserRouter, Route, Switch, NavLink } from "react-router-dom";
 import "./Navbar.sass";
 
 /*
-      loc_navBarTitle: String passed to object for the title
-      navbarItems: Visibility of the search, user and (bars / cross) icons.
-    */
+  loc_navBarTitle: String passed to object for the title
+navbarItems: Visibility of the search, user and (bars / cross) icons.
+*/
 class Navbar extends Component {
   render() {
     return (
 <nav className="navbar borderRed navbar-expand-lg navbar-dark bg-dark">
-  {this.props.navbarItems[0] === true ?<NavLink to="/"><div className="fas"><i className="fas fa-chevron-circle-left"/></div></NavLink>:null}
+  {this.props.navbarItems[0] === true ?<NavLink to="/"><div className="fas"><i class="fas fa-chevron-circle-left"></i></div></NavLink>:null}
   <a className="navbar-brand" href="#">{this.props.titleFromParent}</a>
-    <ul className="nav navbar-right ml-auto">     
-      {this.props.navbarItems[1] === true ? <li><i className="fas fa-search"/></li> : null}
-      {this.props.navbarItems[2] === true ? <li><i className="fas fa-user-circle"/></li> : null}
-      {this.props.navbarItems[3] === true ? <li><i className="fas fa-bars"/></li> : null}
-      {this.props.navbarItems[3] === false ? <i className="fas fa-times"/> : null}
+    <ul className="nav navbar-right ml-auto">
+      {this.props.navbarItems[1] === true ? <li><i class="fas fa-search"></i></li> : null}
+      {this.props.navbarItems[2] === true ? (<NavLink to="/login"><li><i class="fas fa-user-circle"></i></li> </NavLink>): null}
+      {this.props.navbarItems[3] === true ? <li><i class="fas fa-bars"></i></li> : null}
+      {this.props.navbarItems[3] === false ? <i class="fas fa-times"></i> : null}
     </ul>
 </nav>
 
