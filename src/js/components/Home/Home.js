7db6a09a3ab579ea303d1e74641eb9775dd1bc2b
MrMarcelM <mmyburg@retrorabbit.co.za>
2019-02-07 22:12:24 +0200

    Globe rotation fix to rotate upon onclick.

--- src/js/components/Home/Home.js
+++ src/js/components/Home/Home.js
@@ -1,32 +1,27 @@
 import React, { Component } from "react";
-import ArticleApp from "../App";
 import Mapbuttons from "./Countries";
-import Map from "./Map";
-import MyWorker from "../../other/workerz";
 import Navbar from "../Navbar/Navbar";
-import { BrowserRouter, Route, Switch, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import "./Home.scss";
 
 class Home extends Component {
+
   constructor() {
     super();
     this.state = { center: [0, 0] };
   }
-  changeCenter = center => () => {
-    this.setState({ center });
-  };
+
   render() {
     var loc_navBarTitle = "KnowYourNation";
     var loc_navbarItems = [false, true, true, false];
 
     return (
-      <div className="Home">
-      <Navbar titleFromParent={loc_navBarTitle} navbarItems={loc_navbarItems}/>
-      <h4> Home Component </h4>
-
-      <NavLink to="/Books"> <button className="btn btn-success"> Books </button> </NavLink>
-      <h4> Insert speedy picture here </h4>
-      <Mapbuttons />
-      {/*<ArticleApp/>*/}
+      <div className="home">
+        <Navbar titleFromParent={loc_navBarTitle} navbarItems={loc_navbarItems}/>
+        <NavLink to="/Books">
+          <button className="btn btn-success"> Books </button>{" "}
+        </NavLink>
+        <Mapbuttons />
       </div>
     );
   }
