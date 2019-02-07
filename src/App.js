e1864783d96e759c7bbf8c93fd0118ec4e7885d7
NathanRetroRabbit <nngobale@retrorabbit.co.za>
2019-02-07 10:30:58 +0200

    Added User registration

--- src/App.js
+++ src/App.js
@@ -13,12 +13,61 @@ import SignUp from "./js/components/RegisterPage/SignUp";
 import ForgotSignIn from "./js/components/ForgotSignIn/ForgotSignIn";
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+
+  componentDidMount(){
+    fetch('http://ereader.retrotest.co.za/api/Db')
+    .then(res => res.json())
+    .then(json => {
+      this.setState({
+        isLoaded: true,
+        items: json,
+      })
+    });
+
+    fetch('http://ereader.retrotest.co.za/api/Db', {
+  method: 'POST',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    "usersId": 0,
+    "name": "NA",
+    "surname": "NA",
+    "email": "NA",
+    "phoneNumber": "NA",
+    "password": "NA",
+    "membershipType": "NA",
+  })
+})
+  }
+
   render() {
     var loc_navBarTitle = "KnowYourNation";
     var loc_navbarItems = [false, true, true, true];
 
+    var {isLoaded, items} = this.state;
+
+    if (!isLoaded){
+      return <div>Loading...</div>;
+    }
+    else
     return (
+
       <div className="App">
+            {items.map(item => (
+              <li key={item.id}>
+                {item.name}
+              </li>
+        ))}
         <BrowserRouter>
           <div>
             <Switch>
