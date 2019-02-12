d0baede4b626f83e73c84a6459c1a717e9c9392b
NathanRetroRabbit <nngobale@retrorabbit.co.za>
2019-02-12 14:08:30 +0200

    Added Registration integrated to front end.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
+
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 import store from "./js/store/index";
 import App from "./App.js";
+
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 import * as serviceWorker from './serviceWorker';
 import 'react-flags-select/scss/react-flags-select.scss';
 
