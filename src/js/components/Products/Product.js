5ed23b8098ffce48b2829ad37de4334173542617
Mpilo Mshengu <mmshengu@retrorebbit.co.za>
2019-02-15 11:47:40 +0200

    FrentEnd UI and Can take data from frontend to the database.

--- src/js/components/Products/Product.js
+++ src/js/components/Products/Product.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 import Navbar from "../Navbar/Navbar";
 import "./Product.scss";
-import {storage} from  "../firebase"
-
-
 class Product extends Component {
     constructor(props){
         super(props);
@@ -15,60 +12,26 @@ class Product extends Component {
             Type:'',
             SizeOption:'',
             ColourOption:'' ,
-            image:"",
-            url:'', 
             small:false,
             medium:false,
             large:false,
-            speedy:false,
+            Speedy:false,
             color:false,
             kyn:false,
-            count:10
+            count:0
 
         }
-
-        this.handleChange=this.handleChange.bind(this)
         this.onChange=this.onChange.bind(this);
-        this.handleUpload= this.handleUpload.bind(this);
         this.onSubmit=this.onSubmit.bind(this);
         this.Small=this.Small.bind(this);
         this.Medium=this.Medium.bind(this);
         this.Large=this.Large.bind(this);
-        this.Speedy=this.Speedy.bind(this);
-        this.KYN= this.KYN.bind(this);
-        this.Color=this.Color.bind(this);
+        this.handleChecked=this.handleChecked.bind(this);
+        this.Color=this.Color.bind(this)
     }
-    handleUpload=()=>{
-        const {image}= this.state;
-        const uploadTask =storage.ref(`images/${image.name}`).put(image)
-        uploadTask.on('state_changed',
-        (snapshot)=>{
-//progress function......
-        },(error)=>{
-            console.log(error)
-            //error function
-        },()=>{
-            //complete function...
-            storage.ref('images').child(image.name).getDownloadURL().then(url=>{
-                this.setState(url)
-            })
-        })
-    }
-    handleChange=e=>{
-        
-        if(e.target.files[0]){
-            const {image}= e.target.files[0];
-            this.setState(()=>({image}));
-        }
-    }
-    //checkbox
-    handleChecked () {
-        this.setState({isChecked: !this.state.isChecked});
-      }
     onChange(e){
         this.setState({[e.target.name]:e.target.value});
     }
-
     onSubmit(e){
         e.preventDefault();
         const post={
@@ -88,9 +51,7 @@ class Product extends Component {
             },
             body:JSON.stringify(post)
         })
-        .then(response=>response.json())
-        .then(data=>console.log(data));        
-
+        .then(response=>response.json());
     }
     Color(){
         if(this.state.color==false){
@@ -102,42 +63,27 @@ class Product extends Component {
         }
 
     }
-    Speedy(){
+    handleChecked(){
         if(this.state.speedy==false){
-            this.setState({speedy:true});
-            this.setState({Type:"Speedy"})
-         
+            this.setState({speedy:true,kyn:false,Type:"Speedy"});
         }
         else{
-            this.setState({speedy:false});
+            this.setState({speedy:false,kyn:true,Type:"Know your nation"});
         }
-
     }
-    KYN(){
-        if(this.state.kyn==false){
-            this.setState({kyn:true});
-            this.setState({Type:"Know your nation"})
-         
-        }
-        else{
-            this.setState({nyk:false});
-        }
-
-    }
-      Small(){
-        if(this.state.small==false){
+    Small()
+    {
+        if(this.state.small==false)
+        {
             this.setState({small:true});
-            this.setState({count:1});
-         
+            this.setState({count:1}); 
         }
         else{
-            this.setState({small:false});
-            
+            this.setState({small:false}); 
         }
-      }
-
-      Medium(){
-
+    }
+    Medium()
+    {
         if(this.state.medium==false){
             this.state.medium=true;
             this.setState({count:2})
@@ -145,26 +91,20 @@ class Product extends Component {
         else{
             this.state.medium=false;
         }
-      }
-      Large(){
+    }
+    Large(){
         if(this.state.large==false){
             this.setState({large:true});
-            this.setState({count:3})
-
+            this.setState({count:3});
         }
         else{
             this.setState({large:false});
         }
-
-      }
-      
-    
+      } 
     render()
     {
-        
         let loc_navBarTitle = "CREATE PRODUCT";
         let loc_navbarItems = [true, false, false, true];
-
         return (
             <div className="Product">
                 <Navbar titleFromParent={loc_navBarTitle} navbarItems={loc_navbarItems}/>
@@ -184,7 +124,9 @@ class Product extends Component {
                         </div>
                         <div className="col-12 marginTop20px">
                             <label >Name</label>
-                            <span><input className="formInput" name="Name" type="text" onChange={this.onChange}  value={this.state.Name}/></span>
+                            <span>
+                                <input className="formInput" name="Name" type="text" onChange={this.onChange}  value={this.state.Name}/>
+                            </span>
                         </div>
                         <div className="col-12 marginTop20px">
                             <label >Description</label>
@@ -197,8 +139,8 @@ class Product extends Component {
                         </div>
                         <div className="col-12 marginTop10px">
                             <span className ="rad">
-                                <input type="radio" name="Speedy" onClick={this.Speedy}  value={this.state.Speedy}/><label >Speedy</label>
-                                <input type="radio" value="Know your nation" name="Speedy" onClick={this.KYN}/> Know your nation
+                                <input type="radio" name="Speedy" value={this.state.Speedy} onClick={this.handleChecked}  /><label >Speedy</label>
+                                <input type="radio" value="Know your nation" name="Speedy" value={this.state.Speedy} onClick={this.handleChecked}/> Know your nation
                             </span>
                         </div>
                         <div className="col-12 marginTop20px">
@@ -226,22 +168,19 @@ class Product extends Component {
                         <div className="col-12 marginTop10px">
                             <div className ="color">
                                 <input  type="radio" id="small" onClick={this.Color}  name="colorRadio"  />
-                                <input  type="radio"  Id="medium" name="colorRadio"  />
+                                <input  type="radio"  id="medium" name="colorRadio"  />
                                 <input  type="radio" id="large"  name="colorRadio"/>
                             </div>
                         </div>
                         <div className="col-12 marginTop20px">
                             <span className="createButton" >
-
                                 <button onClick ={this.onSubmit} type="button" class="btn btn-success">CREATE</button>
                             </span>
                         </div>
-
                     </div>
                 </div>
             </div>
         );
     }
 }
-
 export default Product;
