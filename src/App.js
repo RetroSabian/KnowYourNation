775a4a171fe0121816e16e0d90d3732f0ea1d193
sjumba <46775605+sjumba@users.noreply.github.com>
2019-02-18 09:58:59 +0200

    Unstyled Donation pages,  I have pushed them since I am handing the task over to Kyle.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
 import React, { Component } from "react";
+import logo from "./logo.svg";
 import "./App.scss";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import AdminMembership from "./js/components/AdminMembershipPage/AdminMembership"
+import {BrowserRouter, Route, Switch, NavLink } from "react-router-dom";
+import "./App.sass";
+import Dummy from "./js/components/Dummy";
 import Home from "./js/components/Home/Home";
+import Navbar from "./js/components/Navbar/Navbar";
+import { Provider } from "react-redux";
 import Books from "./js/components/Books/Books";
 import Login from "./js/components/LoginPage/Login";
 import SignUp from "./js/components/RegisterPage/SignUp";
+
 import ForgotSignIn from "./js/components/ForgotSignIn/ForgotSignIn";
-import ArticleReader from "./js/components/Articles/ArticleReader";
-import VideoPlayer from "./js/components/Articles/VideoPlayer";
-import AdminMembershipDisplay from "./js/components/AdminMembershipPage/AdminMembershipDisplay";
 import main from "./js/components/Main_Menu/Main";
 import collection from "./js/components/CollectionsPage/Collection";
+import donation from "./js/components/DonationPage/Donation";
+import volunteer from "./js/components/VolunteerPage/Volunteer";
+import donationSuccess from "./js/components/DonationPage/DonationSuccess";
+import store from "./js/components/StorePage/Store";
 class App extends Component {
   render() {
     var loc_navBarTitle = "KnowYourNation";
     var loc_navbarItems = [true, true, true, true];
+
     return (
-      <div className="app">
+      <div className="App">
         <BrowserRouter>
+          <div>
             <Switch>
               <Route path="/login" component={Login} />
               <Route path="/register" component={SignUp} />
               <Route path="/resetpassword" component={ForgotSignIn} />
-               <Route path="/collection" component={collection} />
+              <Route path="/Books" component={Books} />
+              <Route path="/collection" component={collection} />
+              <Route path="/donation" component={donation} />
+              <Route path="/volunteer" component={volunteer}/>
+              <Route path = "/donationSuccess" component={donationSuccess}/>
+              <Route path="/store" component={store} />
               <Route path="/main" component={main}/>
-              <Route path="/books" component={Books} />
-              <Route path="/ereader" component={ArticleReader} />
-              <Route path="/videoplayer" component={VideoPlayer} />
-              <Route path="/AdminMembership" component={AdminMembership}/>
-              <Route path="/AdminMembershipDisplay" component={AdminMembershipDisplay}/>
               <Route component={Home} />
             </Switch>
+          </div>
         </BrowserRouter>
       </div>
     );
